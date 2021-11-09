6dfdb870a17fa38375be7a27c0877f879366a8d9
Gabriel Miranda <70436348+Gabriel090699@users.noreply.github.com>
2021-11-09 20:39:17 -0300

    menu

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ from lista import Agenda
 
 #gabriel
 
-c = Agenda()
-c.InserePessoa('Erike', 21)
-c.InserePessoa('Gabriel', 20)
-c.imprime()
+AgendaDePessoas = Agenda()
+
+opcao = True
+while(opcao):
+    print("|-----------------------------------------------------------|\n")
+    print("|    1 - Criar Agenda                                       |\n")
+    print("|    6 - Imprimir Agenda                                    |\n")
+    print("|    7 - Sair                                               |\n")
+    print("|-----------------------------------------------------------|\n")
+    op = int(input("Opcao:"))
+    if op == 1:
+        nome = input("Digite o nome da pessoa:")
+        idade = int(input(f"{nome} digite sua idade:"))
+        AgendaDePessoas.InserePessoa(nome, idade)
+    elif op == 6:
+        AgendaDePessoas.imprime()
+    elif op == 7:
+        op = False
